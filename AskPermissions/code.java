ae42c5de97c46f81ccd0e412de727a71df0419f8
Eduards Bistrums <eduards.bistrums@gmail.com>
2016-08-09 17:23:30 +0300

    Update code.java

--- AskPermissions/code.java
+++ AskPermissions/code.java
@@ -1,15 +1,3 @@
-
-// Ask for all permissions at once. If any permission 
-// is denyed the application will exit. If all permissions
-// granted continue with MainActivity
-//
-// Basic logic:
-//      1. Thnk of any number for               PERMISSION_ACCESS_CODE
-//      2. Check permissions                    checkPermissions()
-//      3. Receive answer                       onRequestPermissionsResult()
-//      4. Exit if missing permissin            tellApplicationNeedPermission()
-//      5. Success if all permissins granted    allPermissionsGranted()
-
 public class AskPermissionActivity extends AppCompatActivity {
   
     // Any number for callback. Change as you wish!
